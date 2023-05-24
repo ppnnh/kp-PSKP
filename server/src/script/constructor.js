d14e6f8a4320fc234fd3f27e7e85866199b2fa96
lerochka <2pitsaaaa@gmail.com>
2023-05-24 21:12:41 +0300

    updated

--- server/src/script/constructor.js
+++ server/src/script/constructor.js
@@ -1,70 +1,93 @@
-let totalPrice=5
-let totalWeight=100
+let totalPrice = 5;
+let totalWeight = 100;
 
+function insertIngredient(e) {
+	let ingredient = e.getAttribute('class');
+	let tag = document.createElement('img');
+	tag.src = `/static/ingredient/${ingredient}.png`;
+	tag.id = `${ingredient}`;
+	tag.className = 'constructor-image';
+	tag.width = '300';
+	let sauce = document.getElementById(`Sauce`);
+	let cheese = document.getElementById(`Cheese`);
+	if (
+		document.getElementById(`${ingredient}`) == undefined &&
+		sauce == undefined &&
+		cheese == undefined
+	) {
+		base.after(tag);
+		totalPrice += parseFloat(
+			document.getElementById(`price ${ingredient}`).textContent
+		);
+		totalWeight += parseFloat(
+			document.getElementById(`weight ${ingredient}`).textContent
+		);
 
-function insertIngredient(e){
-    let ingredient=e.getAttribute("class")
-    let tag=document.createElement("img")
-    tag.src=`/static/ingredient/${ingredient}.png`
-    tag.id=`${ingredient}`
-    tag.style="position:absolute"
-    tag.width="300"
-    let sauce=document.getElementById(`Sauce`)
-    let cheese=document.getElementById(`Cheese`)
-    if (document.getElementById(`${ingredient}`)==undefined && sauce==undefined && cheese==undefined){
-        base.after(tag)
-        totalPrice+=parseFloat(document.getElementById(`price ${ingredient}`).textContent)
-        totalWeight+=parseFloat(document.getElementById(`weight ${ingredient}`).textContent)
-    
-        totalPrice=Number(totalPrice.toFixed(1))
-        document.getElementById("totalPrice").innerHTML=`Price: ${totalPrice}`
-        document.getElementById("totalWeight").innerHTML=`Weight: ${totalWeight}`
-    } else if (document.getElementById(`${ingredient}`)==undefined && sauce!==undefined && cheese==undefined){
-        sauce.after(tag)
-        totalPrice+=parseFloat(document.getElementById(`price ${ingredient}`).textContent)
-        totalWeight+=parseFloat(document.getElementById(`weight ${ingredient}`).textContent)
-        totalPrice=Number(totalPrice.toFixed(1))
+		totalPrice = Number(totalPrice.toFixed(1));
+		document.getElementById('totalPrice').innerHTML = `Price: ${totalPrice}`;
+		document.getElementById('totalWeight').innerHTML = `Weight: ${totalWeight}`;
+	} else if (
+		document.getElementById(`${ingredient}`) == undefined &&
+		sauce !== undefined &&
+		cheese == undefined
+	) {
+		sauce.after(tag);
+		totalPrice += parseFloat(
+			document.getElementById(`price ${ingredient}`).textContent
+		);
+		totalWeight += parseFloat(
+			document.getElementById(`weight ${ingredient}`).textContent
+		);
+		totalPrice = Number(totalPrice.toFixed(1));
 
-    
-        document.getElementById("totalPrice").innerHTML=`Price: ${totalPrice}`
-        document.getElementById("totalWeight").innerHTML=`Weight: ${totalWeight}`
-    } else if (document.getElementById(`${ingredient}`)==undefined && ingredient=="Sauce"){
-        cheese.before(tag)
-        totalPrice+=parseFloat(document.getElementById(`price ${ingredient}`).textContent)
-        totalWeight+=parseFloat(document.getElementById(`weight ${ingredient}`).textContent)
-        totalPrice=Number(totalPrice.toFixed(1))
+		document.getElementById('totalPrice').innerHTML = `Price: ${totalPrice}`;
+		document.getElementById('totalWeight').innerHTML = `Weight: ${totalWeight}`;
+	} else if (
+		document.getElementById(`${ingredient}`) == undefined &&
+		ingredient == 'Sauce'
+	) {
+		cheese.before(tag);
+		totalPrice += parseFloat(
+			document.getElementById(`price ${ingredient}`).textContent
+		);
+		totalWeight += parseFloat(
+			document.getElementById(`weight ${ingredient}`).textContent
+		);
+		totalPrice = Number(totalPrice.toFixed(1));
 
-    
-        document.getElementById("totalPrice").innerHTML=`Price: ${totalPrice}`
-        document.getElementById("totalWeight").innerHTML=`Weight: ${totalWeight}`
-    } else if(document.getElementById(`${ingredient}`)==undefined){
-        cheese.after(tag)
-        totalPrice+=parseFloat(document.getElementById(`price ${ingredient}`).textContent)
-        totalWeight+=parseFloat(document.getElementById(`weight ${ingredient}`).textContent)
-        totalPrice=Number(totalPrice.toFixed(1))
+		document.getElementById('totalPrice').innerHTML = `Price: ${totalPrice}`;
+		document.getElementById('totalWeight').innerHTML = `Weight: ${totalWeight}`;
+	} else if (document.getElementById(`${ingredient}`) == undefined) {
+		cheese.after(tag);
+		totalPrice += parseFloat(
+			document.getElementById(`price ${ingredient}`).textContent
+		);
+		totalWeight += parseFloat(
+			document.getElementById(`weight ${ingredient}`).textContent
+		);
+		totalPrice = Number(totalPrice.toFixed(1));
 
-    
-        document.getElementById("totalPrice").innerHTML=`Price: ${totalPrice}`
-        document.getElementById("totalWeight").innerHTML=`Weight: ${totalWeight}`
-    }
-
-   
+		document.getElementById('totalPrice').innerHTML = `Price: ${totalPrice}`;
+		document.getElementById('totalWeight').innerHTML = `Weight: ${totalWeight}`;
+	}
 }
 
-function deleteIngredient(e){
-    let ingredient=e.getAttribute("class")
+function deleteIngredient(e) {
+	let ingredient = e.getAttribute('class');
 
-    let tag=document.getElementById(`${ingredient}`)
-    if (tag==undefined){
-    } else {
-        tag.remove()
+	let tag = document.getElementById(`${ingredient}`);
+	if (tag == undefined) {
+	} else {
+		tag.remove();
 
-        totalPrice-=parseFloat(document.getElementById(`price ${ingredient}`).textContent)
-        totalWeight-=parseFloat(document.getElementById(`weight ${ingredient}`).textContent)
+		totalPrice -= parseFloat(
+			document.getElementById(`price ${ingredient}`).textContent
+		);
+		totalWeight -= parseFloat(
+			document.getElementById(`weight ${ingredient}`).textContent
+		);
 
-        document.getElementById("totalPrice").innerHTML=`Price: ${totalPrice}`
-        document.getElementById("totalWeight").innerHTML=`Weight: ${totalWeight}`
-    }
-
-    
+		document.getElementById('totalPrice').innerHTML = `Price: ${totalPrice}`;
+		document.getElementById('totalWeight').innerHTML = `Weight: ${totalWeight}`;
+	}
 }
